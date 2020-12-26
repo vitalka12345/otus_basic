7f7ff4e95f87178981d1d90caaec86129116398f
Vitaliy Komarov <vitaliy932@yandex.ru>
2020-12-27 02:56:59 +0300

    complete homework1

--- 3.git.shell/main.py
+++ 3.git.shell/main.py
@@ -1,6 +1,6 @@
 from time import time
 from functools import wraps
-# import trace
+import decimal
 
 # Degree function
 random_list = range(0, 20)
@@ -11,12 +11,10 @@ def degree_func(income_list, income_degree=2):
     new_list = []
     for i in income_list:
         new_list.append(pow(i, income_degree))
-    print('Degree list:', new_list)
+    print(f"Degree {income_degree} list:", new_list)
 
 
-degree_func(random_list, random_degree)
-
-# Even, Odd, Prime function.
+# Even, Odd function
 operation_list = range(-5, 20)
 
 
@@ -28,18 +26,19 @@ def even_odd_prime_numbers_function(list1, op_type=None):
     count_elem = len(operation_list)
     even_odd_prime_list = []
     for n in range(count_elem):
-        if op_type == 'EVEN':
+        if op_type == "EVEN":
             even_odd_prime_list = filter(lambda x: x % 2 == 0, operation_list)
             returned_list = list(set(even_odd_prime_list))
-        elif op_type == 'ODD':
+        elif op_type == "ODD":
             even_odd_prime_list = filter(lambda x: x % 2 == 1, operation_list)
             returned_list = list(set(even_odd_prime_list))
-        elif op_type == 'PRIME':
+        elif op_type == "PRIME":
             even_odd_prime_list = filter(is_prime, operation_list)
             returned_list = list(set(even_odd_prime_list))
-    print(f"{op_type} list:", returned_list)
+    print(f"{op_type} {operation_list.__repr__()} list:", returned_list)
 
 
+# Prime function
 def is_prime(n):
     d = 2
     while n % d != 0 and n > d:
@@ -47,74 +46,60 @@ def is_prime(n):
     return d == n
 
 
-even_odd_prime_numbers_function(operation_list, 'PRIME')
-
-
-# - создать декоратор для замера времени выполнения функции
-# 2.
-# БОНУСНОЕ ЗАДАНИЕ: создать декоратор, который показывает вложенные входы в функцию.
-# Применить на примере вычисления чисел Фибоначчи
-# https://gist.github.com/mahenzon/d77361a1bd44f138e706ae4734007ee6
-# сложить это всё в один файл. В файле написать код, который продемонстрирует работу всех функций выше.
-# Красивые принты приветствуются. Подсказка: можно воспользоваться модулем pprint
-# - создание декоратора для замера времени выполнения функции - 1 балл
-# внутри обёртки используется wraps из functools
-# декоратор trace - 1 балл
-# написана функция для вычисления чисел Фибоначчи
-# функция для вычисления чисел Фибоначчи использует рекурсию
-# обёртка работает и показывает погружения
-# для всех написанных функций есть пример их работы - 1 балл
-# данные выводятся красиво и понятно - 1 балл
-
-
+# Time call deco
 def time_call(func):
-    print("incoming func:", func.__name__)
-
     @wraps(func)
-    def wrapper(p):
+    def wrapper(*args, **kwargs):
         start = time()
-        res = func(p)
+        res = func(*args, **kwargs)
         end = time()
-        print("res for", p, " = ", res)
         time_taken = end - start
-        print(f"time taken: {time_taken:.13f}")
+        print(f"Time on func \"{func.__name__}\" spent: {time_taken:.20f}")
         return res
 
-    # print("id   wrapper", id(wrapper))
-    # print(wrapper.__wrapped__)
-    # print(id(wrapper))
-    # print(id(func))
-    # print(id(wrapper.__wrapped__))
     return wrapper
 
 
-def trace(func):
-    count = 1
+# Trace deco
+def trace(func1):
+    func1.level = 0
 
-    @wraps(func)
-    def wrapper1(d):
-        count += 1
-        res = func(d)
-        print(f"when {type(res)} returned, count = " + str(count))
-        return wrapper1
+    @wraps(func1)
+    def wrapper1(*args, **kwargs):
+        args_str = ", ".join(map(repr, args))
+        kwargs_str = ", ".join(map(lambda k, v: f"{k}={v!r}", kwargs.items()))
+        func_args = list(filter(bool, (args_str, kwargs_str)))
+        print("___" * func1.level + f"-> {func1.__name__}({', '.join(func_args)})")
+        func1.level += 1
+        res = func1(*args, **kwargs)
+        func1.level -= 1
+        print("___" * func1.level + f"<- {func1.__name__}({', '.join(func_args)}) == {res}")
+        return res
+
+    return wrapper1
 
 
-# @trace
-@time_call
+@trace
 def fib(pos):
-    """
-    1, 1, 2, 3, 5, 8
-    1 = 1
-    2 =  1
-    3 =  1
-    5 =  2  || (2 + 1) + 2
-    8 =  3  || 8 = (3 + 2) + 3 || 8 = ((2 + 1) + 2) + (2 + 1)
-    :param pos:
-    :return:
-    """
-    if pos <= 1:
+    if pos < 0:
+        return None
+    if pos < 2:
         return 1
     return fib(pos - 1) + fib(pos - 2)
 
 
-fib(5)
+# Pow function
+@time_call
+def pow_funk(a, b):
+    return decimal.Decimal(pow(a, b))
+
+
+if __name__ == "__main__":
+    degree_func(random_list, random_degree)
+    even_odd_prime_numbers_function(operation_list, "ODD")
+    even_odd_prime_numbers_function(operation_list, "EVEN")
+    even_odd_prime_numbers_function(operation_list, "PRIME")
+    print("Demonstrate trace deco:")
+    fib(4)
+    # print("Demonstrate time call deco pow func = ", pow_funk(123456789, 12345))
+    print("Demonstrate time call deco, result pow function = ", format(pow_funk(123456789, 12345), ".1E"))
