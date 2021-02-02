87bd22a96a14df5450564de0a568a9cf5cdb87e9
Vitaliy Komarov <vitaliy932@yandex.ru>
2021-02-03 00:48:37 +0300

    code with error, need try except construction in Nine

--- 8.DataClasses/cars/__init__.py
+++ 8.DataClasses/cars/__init__.py
@@ -1,6 +1,28 @@
 from baseMachine import BaseMachine
 
 
-class Nine(BaseMachine):
+class Car(BaseMachine):
+    type_model = 'Легковой автомобиль'
+    wheel_count = 4
+
+    def riding(self, name):
+        return print(f"{name} is a car and it goes.")
+
+
+class Nine(Car):
     type_model = 'Девятка'
     wheel_count = 4
+
+    def start(self, fuel):
+        if fuel > 10:
+            print("Engine started!")
+        else:
+            print("Not enough fuel!")
+
+    def get_type_model(self, item):
+        return item
+
+    def riding(self, name):
+        return print(f"{name} is a car and it goes.")
+
+
