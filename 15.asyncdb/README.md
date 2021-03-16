2846c1e7361ea5f294c3908b0bc57319e2774e25
Vitaliy Komarov <vitaliy932@yandex.ru>
2021-03-17 00:07:50 +0300

    completed 3 hw, and added readme about hw3

--- 15.asyncdb/README.md
+++ 15.asyncdb/README.md
@@ -2,4 +2,6 @@
 
 > docker run -e POSTGRES_USER=user -e POSTGRES_PASSWORD=password -e POSTGRES_DB=my_db -p 5432:5432 postgres
 
-Для успешного записи в БД длинных строк нужно использовать тип varchar.
+В домашнем задании создаётся таблица posts с колонками (id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+userId INTEGER NOT NULL, title VARCHAR DEFAULT '', body VARCHAR DEFAULT ''). С открытого апи
+https://jsonplaceholder.typicode.com/posts выкачиваются посты пользователей и сохраняются в БД.
--- 15.asyncdb/main.py
+++ 15.asyncdb/main.py
@@ -1,19 +1,3 @@
-# Асинхронная работа с сетью
-# Цель: В этом ДЗ вы напишите скрипт.
-# написать скрипт, который при помощи асинхронного клиента обращается к открытому апи
-# (например, jsonplaceholder) и выводит оттуда информацию
-# создать модели для объектов, которые тянутся с открытого апи
-# записать вытянутые модели в БД
-# скрипт для стягивания данных и записи в БД реализован в асинхронном виде
-# Критерии оценки: у моделей есть primary_key - 1 балл
-# созданы все миграции - 1 балл
-# скрипт стягивает данные с API и складывает в БД - 1 балл
-# обращение к API выполняется в асинхронном виде - 1 балл
-# на асинхронный клиент применяется close при завершении работы - 1 балл
-# запись в базу данных выполняется в асинхронном виде - 1 балл
-# соединение с базой данных закрывается при завершении работы - 1 балл
-# Рекомендуем сдать до: 26.02.2021
-# docker run -e POSTGRES_USER=user -e POSTGRES_PASSWORD=password -e POSTGRES_DB=my_db -p 5432:5432 postgres
 import asyncio
 import asyncpg
 import aiohttp
@@ -33,12 +17,15 @@ async def run():
         host='127.0.0.1',
     )
 
+    await conn.execute('''
+        CREATE TABLE public.posts (id SERIAL PRIMARY KEY NOT NULL UNIQUE,
+        userId INTEGER NOT NULL, title VARCHAR DEFAULT '', body VARCHAR DEFAULT '');
+        ''')
+
     for i in await get_posts():
-        print("id: ", i["id"], "userId: ", i["userId"], "title: ", str(i["title"]),
-              "body: ", str(i["body"]))
         await conn.execute(
             """
-            INSERT INTO public.posts("userId", title, body)
+            INSERT INTO public.posts(userId, title, body)
             VALUES ($1, $2, $3);
             """,
             i["userId"],
@@ -50,3 +37,5 @@ async def run():
 
 if __name__ == '__main__':
     asyncio.run(run())
+    loop = asyncio.get_event_loop()
+    loop.close()
